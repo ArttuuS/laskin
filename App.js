9193f32113c3935edc6b66750e2f5a3addfa663e
ArttuuS <arttu.saily@gmail.com>
2024-01-23 16:44:40 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [result, setResult] = useState("");
+  const [numberOne, setNumberOne] = useState("");
+  const [numberTwo, setNumberTwo] = useState("");
+
+  const showPlus = () => {
+    setResult(String(Number(numberOne) + Number(numberTwo)));
+  };
+
+  const showMinus = () => {
+    setResult(String(Number(numberOne) - Number(numberTwo)));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Result: <Text>{result}</Text>
+      </Text>
+      <TextInput
+        style={{ width: 200, borderColor: "grey", borderWidth: 1 }}
+        onChangeText={(text) => setNumberOne(text)}
+      />
+      <TextInput
+        style={{ width: 200, borderColor: "grey", borderWidth: 1 }}
+        onChangeText={(text) => setNumberTwo(text)}
+      />
+      <View style={{ flexDirection: "row" }}>
+        <TouchableOpacity style={styles.button} onPress={showPlus}>
+          <Text>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={showMinus}>
+          <Text>-</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +51,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "lightblue",
+    padding: 10,
+    marginHorizontal: 5,
+    marginTop: 10,
   },
 });
